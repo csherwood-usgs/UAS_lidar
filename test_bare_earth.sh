1bc2edbc7c2b74fb0ccf5f0855b7c792f33de42e
Chris Sherwood <csherwood@usgs.gov>
2024-04-15 18:19:00 -0400

    still working on bare earth

--- test_bare_earth.sh
+++ test_bare_earth.sh
@@ -15,9 +15,10 @@ TEMP_DIR1="temp_tiles"
 #OUTFILE="2023-028-FA_Sandwich_Ricoh_SfM_ptCloud_UTM19N_NAVD88_5cm_opt.laz"
 
 NUM_CORES=8
-STEP=0.25
+STEP_XY=0.25
+STEP_Z=0.25
 TILE_SIZE=512
-
+BUFFER=30
 
 # clobber any existing files in the temp folders
 rm -rf temp_tiles
@@ -30,8 +31,8 @@ mkdir -p temp_tiles_denoised
 # tile....with TILE_SIZE=512, there are about 11 files with up to 500 MB each
 # have not tested adding buffers or making tiles smaller
 "${LASPATH}lastile64" -i "${INDIR}${INFILE_BASE}.laz" \
-    -v -tile_size ${TILE_SIZE} \
-    -o "temp_tiles/tile" -olaz
+     -v -tile_size ${TILE_SIZE} -buffer ${BUFFER} -flag_as_withheld \
+     -o "temp_tiles/tile" -olaz
 
 # run lasnoise on all temporary tiles on multiple cores while
 # aggressively filtering noise points in an attempt to get as
@@ -39,15 +40,15 @@ mkdir -p temp_tiles_denoised
 # classification of points above the grounds as noise does not
 # matter here.
 "${LASPATH}lasnoise64" -i "temp_tiles/tile*.laz" \
-  -step_xy 0.25 -step_z 0.25 -classify_as 7 -v -lowest -odir temp_tiles_denoised -olaz \
+  -step_xy 0.25 -step_z 0.25 -classify_as 7 -v -odir temp_tiles_denoised -olaz \
   -cores ${NUM_CORES}
 
 rm -rf temp_tiles_thinned
 mkdir -p temp_tiles_thinned
 
-:: run lasthin on all temporary tiles on multiple cores to mark
-:: the point closest to the fifth (5th) lowest percentile in each
-:: .25 meter by .25 meter cell with temporary classification code 6
+# run lasthin on all temporary tiles on multiple cores to mark
+# the point closest to the fifth (5th) lowest percentile in each
+# .25 meter by .25 meter cell with temporary classification code 6
 "${LASPATH}lasthin64" -i "temp_tiles_denoised/tile*.laz" \
   -ignore_class 7 -step 0.25 -percentile 0.05 -classify_as 6 -v -odir temp_tiles_thinned -olaz \
   -cores ${NUM_CORES}
@@ -59,11 +60,7 @@ mkdir -p temp_tiles_ground
   -cores ${NUM_CORES}
 
 "${LASPATH}lasmerge64" -i "temp_files_ground/tile*.laz" \
-   -v  \
-   -o "${OUTDIR}${INFILE_BASE}_ground.laz"
-
-
-# remove temp dir
-# rm -rf  ${TEMP_DIR}
+   -v  -drop_withheld -keep_class 2 \
+   -o "${OUTDIR}${INFILE_BASE}_ground.laz" -olaz
 
 
